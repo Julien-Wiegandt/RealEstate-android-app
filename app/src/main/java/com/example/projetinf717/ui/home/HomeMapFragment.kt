333beb788260f266db71b93ae7083f1b49ad67d6
Anna <spatchoun@gmail.com>
2021-12-09 09:11:35 -0500

    Modification de notre bouton de position

--- app/src/main/java/com/example/projetinf717/ui/home/HomeMapFragment.kt
+++ app/src/main/java/com/example/projetinf717/ui/home/HomeMapFragment.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,12 +21,23 @@ import com.google.android.gms.maps.model.MarkerOptions
 import java.io.IOException
 import java.util.*
 import android.app.Activity
+import android.content.Context
 
 import androidx.core.app.ActivityCompat
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+import android.graphics.drawable.Drawable
+
+import com.google.android.gms.maps.model.BitmapDescriptor
+
+
+
 
 
 class HomeMapFragment : Fragment(), OnMapReadyCallback {
@@ -74,6 +86,12 @@ class HomeMapFragment : Fragment(), OnMapReadyCallback {
                     marker = MarkerOptions()
                         .position(latLng )
                         .title("Ma position")
+                        .icon(context?.let {
+                            BitmapFromVector(
+                                it,
+                                R.drawable.ic_baseline_my_location_24
+                            )
+                        })
                     mMap.addMarker(marker)
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.position, 12F))
                 }else{
@@ -84,6 +102,36 @@ class HomeMapFragment : Fragment(), OnMapReadyCallback {
 
     }
 
+    private fun BitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        // below line is use to generate a drawable.
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+        // below line is use to add bitmap in our canvas.
+        val canvas = Canvas(bitmap)
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas)
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
     fun checkAndRequestPermissions(): Boolean {
         val internet = context?.let {
             ContextCompat.checkSelfPermission(
